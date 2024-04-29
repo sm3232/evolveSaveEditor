ffa798523adaadd5ff224eb9bad3d94fd76c66a5
sm3232 <connor.hill@mymail.champlain.edu>
2024-04-29 14:05:11 -0400

    search

--- script.js
+++ script.js
@@ -3,40 +3,45 @@ const output = document.getElementById("save-output");
 const root = document.getElementById("root");
 const copy = document.getElementById("copy-button");
 const paste = document.getElementById("paste-button");
+const searchV = document.getElementById("search-values");
+const searchK = document.getElementById("search-keys");
+const searchResults = document.getElementById("search-results");
 const fgcolor = "#8090c0";
 let saveLoaded = false;
 let data;
+let keys = [];
+let values = [];
 
 const QUERY_MAP = {
-Resources: "resource",
-Evolution: "evolution",
-Tech: "tech",
-City: "city",
-Space: "space",
-Interstellar: "interstellar",
-Portal: "portal",
-Tauceti: "tauceti",
-Civic: "civic",
-Race: "race",
-Genes: "genes",
-Blood: "blood",
-Stats: "stats",
-Events: "event",
-MajorEvents: "m_event",
-Prestige: "prestige",
-Power: "power",
-Support: "support",
-Settings: "settings",
-Queue: "queue",
-ResearchQueue: "r_queue",
-Messages: "lastMsg",
-StarDock: "starDock",
-Galaxy: "galaxy",
-Pillars: "pillars",
-Government: "govern",
-Special: "special",
-Custom: "custom",
-Arpa: "arpa",
+    Resources: "resource",
+    Evolution: "evolution",
+    Tech: "tech",
+    City: "city",
+    Space: "space",
+    Interstellar: "interstellar",
+    Portal: "portal",
+    Tauceti: "tauceti",
+    Civic: "civic",
+    Race: "race",
+    Genes: "genes",
+    Blood: "blood",
+    Stats: "stats",
+    Events: "event",
+    MajorEvents: "m_event",
+    Prestige: "prestige",
+    Power: "power",
+    Support: "support",
+    Settings: "settings",
+    Queue: "queue",
+    ResearchQueue: "r_queue",
+    Messages: "lastMsg",
+    StarDock: "starDock",
+    Galaxy: "galaxy",
+    Pillars: "pillars",
+    Government: "govern",
+    Special: "special",
+    Custom: "custom",
+    Arpa: "arpa",
 };
 
 const traverse = (data) => {
@@ -49,6 +54,29 @@ const traverse = (data) => {
         }
     }
     return myMap;
+}
+const addMaxButtons = () => {
+    const resourceRoot = document.querySelector(".resource");
+    const children = resourceRoot.children;
+    for(let i = 0; i < children.length; i++){
+        if(children.item(i).tagName === "DIV"){
+            let child = document.createElement("button");
+            child.innerText = "Set to max";
+            child.className = "max-button";
+            children.item(i).prepend(child);
+            child.onclick = () => {
+                let amount = document.querySelector(`#${children.item(i).className}-amount`);
+                let max = document.querySelector(`#${children.item(i).className}-max`);
+                if(max.value < 0){
+                    amount.value = amount.innerText = Number(1e20);
+                } else {
+                    amount.value = amount.innerText = max.value = max.innerText;
+                }
+                setThing(amount.id, amount.innerText);
+                updateOutput();
+            }
+        }
+    }
 }
 
 const setThing = (id, val) => {
@@ -76,6 +104,7 @@ const expand = (scope, data) => {
             div.style.padding = "15px";
             div.style.textAlign = "left";
             div.style.margin = "15px";
+            keys.push(section);
             scope.appendChild(div);
             expand(div, v);
         } else {
@@ -89,6 +118,7 @@ const expand = (scope, data) => {
             span.setAttribute("contenteditable", "");
             span.setAttribute("spellcheck", false);
             span.className = "text-field dynamic";
+            values.push(span);
             if(scope !== root){
                 span.id = scope.className + "-" + k;
             } else {
@@ -96,9 +126,11 @@ const expand = (scope, data) => {
             }
 
             span.addEventListener("input", () => {
+                span.value = span.innerText;
                 setThing(span.id, span.innerText);
                 updateOutput();
             })
+            span.value = span.innerText;
             element.style.display = "inline";
             contain.appendChild(element);
             contain.appendChild(span);
@@ -125,6 +157,8 @@ input.addEventListener("input", () => {
         data = JSON.parse(LZString.decompressFromBase64(input.value));
         let map = traverse(data);
         expand(root, map);
+        addMaxButtons();
+
         updateOutput();
         saveLoaded = true;
     } catch(e) {
@@ -169,7 +203,7 @@ const quickActions = {
     Capcraftableresources: () => {
         for(let [k, v] of Object.entries(data.resource)){
             if(v.display && v.max === -1){
-                v.amount = 1e25;
+                v.amount = 1e20;
                 let element = document.getElementById("resource-" + k + "-amount");
                 element.innerText = element.value = v.amount;
             }
@@ -178,7 +212,7 @@ const quickActions = {
     Capspecialresources: () => {
         for(let [k, v] of Object.entries(data.resource)){
             if(v.display && v.max === -2){
-                v.amount = 1e25;
+                v.amount = 1e20;
                 let element = document.getElementById("resource-" + k + "-amount");
                 element.innerText = element.value = v.amount;
             }
@@ -199,3 +233,31 @@ quicks.forEach((e) => {
         }
     })
 })
+
+searchV.addEventListener("input", async () => {
+    searchResults.innerHTML = "";
+    if(searchV.value.length === 0) return;
+    for(let i = 0; i < values.length; i++){
+        const regex = new RegExp(`${searchV.value}`, "gi");
+        if(values[i].value.match(regex)){
+            let p = document.createElement("p");
+            p.innerText = `${values[i].id}: ${values[i].value}`;
+            p.onclick = () => values[i].scrollIntoView({behavior: "smooth"});
+            searchResults.appendChild(p);
+        }
+    }
+})
+searchK.addEventListener("input", async () => {
+    searchResults.innerHTML = "";
+    if(searchK.value.length === 0) return;
+    for(let i = 0; i < keys.length; i++){
+        const regex = new RegExp(`${searchK.value}`, "gi");
+        if(keys[i].innerText.match(regex)){
+            let p = document.createElement("p");
+            p.innerText = keys[i].innerText;
+            p.onclick = () => keys[i].scrollIntoView({behavior: "smooth"});
+            searchResults.appendChild(p);
+        }
+    }
+})
+input.dispatchEvent(new Event("input", {bubbles: true}));
